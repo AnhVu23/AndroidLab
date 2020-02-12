3b6f9f521a7ac42f61a392e53734626f005287cb
Anh Vu <anh.vu@vertics.co>
2020-02-12 11:12:44 +0700

    FIxing notification channel

--- RetrofitLab/app/src/main/java/com/example/retrofitlab/MainActivity.kt
+++ RetrofitLab/app/src/main/java/com/example/retrofitlab/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.retrofitlab
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.NotificationCompat
@@ -18,16 +22,32 @@ class MainActivity : AppCompatActivity(){
             Snackbar.LENGTH_LONG
         ).setAction("OK") { /* do smth */ }
         .show()
-        val notif = NotificationCompat.Builder(this, "1")
-        .setSmallIcon(R.drawable.ic_launcher_background)
-        .setContentTitle(getString(R.string.notify_title))
-        .setContentText("The long description text...")
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .build()
-        NotificationManagerCompat.from(this).notify(1, notif)
-    }
+        createNotificationChannel()
 
+    }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "1",
+                getString(R.string.channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = getString(R.string.channel_description)
+                }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as
+                        NotificationManager
+            notificationManager.createNotificationChannel(channel)
+            }
+        }
     fun updateTextView(text: String, hit: Int) {
+        val notif = NotificationCompat.Builder(this, "1")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(getString(R.string.notify_title))
+            .setContentText("The long description text...")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+        NotificationManagerCompat.from(this).notify(1, notif)
         (supportFragmentManager.findFragmentById(R.id.fragment_detail) as? DetailFragment)
             ?.updateText(text, hit)
     }
