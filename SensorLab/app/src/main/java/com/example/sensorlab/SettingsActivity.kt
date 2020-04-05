803f2db83dc6ad331d60a845efa2621f39fb1884
anhvu-95 <vu.haianh291@gmail.com>
2020-04-05 19:59:20 +0300

    Feat: Adding setting activity

---
+++ SensorLab/app/src/main/java/com/example/sensorlab/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sensorlab
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportFragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment()).commit()
+    }
+}
